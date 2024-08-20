fa3e8f2f2d188537862e3b56783c1112d4c7174a
Keaton Sentak <ksentak01@gmail.com>
2024-08-20 19:06:52 -0400

    remove lodash

---
+++ index.js
@@ -0,0 +1,21 @@
+// Config Variables
+const teams = [
+  'Keaton',
+  'Gavin',
+  'Matt',
+  'Landon',
+  'Harry',
+  'Conor',
+  'Nick',
+  'Chris',
+  'Doemnic',
+  'Joe',
+];
+
+const sendDiscordMsg = false;
+
+const webhookUrl = '';
+
+// Function to randomize draft order
+
+// Function to send discord msg
