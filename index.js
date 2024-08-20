049d324278f03fc4458713270cc07a19f09423c3
Keaton Sentak <ksentak01@gmail.com>
2024-08-20 19:27:50 -0400

    add logic to generate order

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// Config Variables
+/* global fetch */
+
+// Config
 const teams = [
   'Keaton',
   'Gavin',
@@ -8,14 +10,59 @@ const teams = [
   'Conor',
   'Nick',
   'Chris',
-  'Doemnic',
+  'Domenic',
   'Joe',
 ];
 
-const sendDiscordMsg = false;
+const sendDiscordMsg = true;
 
 const webhookUrl = '';
 
 // Function to randomize draft order
+const shuffleDraftOrder = (teamArr) => {
+  // Loop through array from last to first
+  for (let i = teamArr.length - 1; i > 0; i--) {
+    // Pick random index from 0 to i
+    const j = Math.floor(Math.random() * (i + 1));
+    // Swap j and i
+    [teamArr[i], teamArr[j]] = [teamArr[j], teamArr[i]];
+  }
+
+  return teamArr;
+};
+
+// Function to format draft order
+const formatDraftOrder = (formattedArr) => {
+  return formattedArr.map((team, index) => `${index + 1}. ${team}`).join('\n');
+};
 
 // Function to send discord msg
+const sendMessage = async (msg, url) => {
+  const msgObj = {
+    content: '```' + msg + '```',
+  };
+
+  try {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(msgObj),
+    });
+
+    if (res.status === 204) {
+      console.log('Msg sent successfully');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Call functions
+const shuffledTeams = shuffleDraftOrder(teams);
+const formattedTeams = formatDraftOrder(shuffledTeams);
+
+if (sendDiscordMsg && webhookUrl) {
+  sendMessage(formattedTeams, webhookUrl);
+}
